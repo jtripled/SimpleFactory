7fe38e9b54574b253322b0bf50adbea5bb772462
jtripled <jtripled@gmail.com>
2017-09-28 12:04:58 -0500

    fluid packets handle custom fluids

--- src/main/java/com/jtripled/simplefactory/fluid/network/FluidMessage.java
+++ src/main/java/com/jtripled/simplefactory/fluid/network/FluidMessage.java
@@ -2,9 +2,9 @@ package com.jtripled.simplefactory.fluid.network;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 /**
@@ -41,11 +41,11 @@ public class FluidMessage implements IMessage
     public void fromBytes(ByteBuf buf)
     {
         pos = new BlockPos(buf.readInt(), buf.readInt(), buf.readInt());
-        int id = buf.readInt();
         int amount = buf.readInt();
-        if (id != NULL_ID && amount > 0)
+        if (amount > 0)
         {
-            fluid = new FluidStack(getFluidFromID(id), amount);
+            String name = ByteBufUtils.readUTF8String(buf);
+            fluid = new FluidStack(FluidRegistry.getFluid(name), amount);
         }
     }
 
@@ -57,36 +57,13 @@ public class FluidMessage implements IMessage
         buf.writeInt(pos.getZ());
         if (fluid == null)
         {
-            buf.writeInt(NULL_ID);
             buf.writeInt(0);
+            ByteBufUtils.writeUTF8String(buf, "null");
         }
         else
         {
-            buf.writeInt(getIDFromFluid(fluid.getFluid()));
             buf.writeInt(fluid.amount);
+            ByteBufUtils.writeUTF8String(buf, fluid.getFluid().getName());
         }
-    }
-    
-    public static final int NULL_ID = -1;
-    public static final int WATER_ID = 0;
-    public static final int LAVA_ID = 1;
-    
-    public static Fluid getFluidFromID(int id)
-    {
-        switch (id)
-        {
-            case NULL_ID: return null;
-            case WATER_ID: return FluidRegistry.WATER;
-            case LAVA_ID: return FluidRegistry.LAVA;
-            default: return null;
-        }
-    }
-    
-    public static int getIDFromFluid(Fluid fluid)
-    {
-        if (fluid == null) return NULL_ID;
-        if (fluid == FluidRegistry.WATER) return WATER_ID;
-        if (fluid == FluidRegistry.LAVA) return LAVA_ID;
-        return NULL_ID;
     }
 }
