07b794890a637e57941065617b9b47287df48e1e
jtripled <jtripled@gmail.com>
2017-09-29 10:54:00 -0400

    autocrafting table basework

---
+++ src/main/java/com/jtripled/simplefactory/machine/tile/TileAutomaticCraftingTable.java
@@ -0,0 +1,93 @@
+package com.jtripled.simplefactory.machine.tile;
+
+import javax.annotation.Nullable;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+/**
+ *
+ * @author jtripled
+ */
+public class TileAutomaticCraftingTable extends TileEntity implements ITickable
+{
+    private final ItemStackHandler inventory;
+    
+    public TileAutomaticCraftingTable()
+    {
+        this.inventory = new AutomaticCraftingTableItemHandler();
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing)
+    {
+        return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY
+                || super.hasCapability(capability, facing);
+    }
+
+    @Nullable
+    @Override
+    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing)
+    {
+        return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY
+                ? (T)inventory : super.getCapability(capability, facing);
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        compound.setTag("inventory", inventory.serializeNBT());
+        return super.writeToNBT(compound);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        inventory.deserializeNBT(compound.getCompoundTag("inventory"));
+        super.readFromNBT(compound);
+    }
+    
+    @Override
+    public void onDataPacket(NetworkManager network, SPacketUpdateTileEntity packet)
+    {
+        readFromNBT(packet.getNbtCompound());
+    }
+    
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        return new SPacketUpdateTileEntity(getPos(), 1, getUpdateTag());
+    }
+    
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        return writeToNBT(super.getUpdateTag());
+    }
+    
+    @Override
+    public void handleUpdateTag(NBTTagCompound compound)
+    {
+        readFromNBT(compound);
+    }
+
+    @Override
+    public void update()
+    {
+        
+    }
+    
+    public static class AutomaticCraftingTableItemHandler extends ItemStackHandler
+    {
+        public AutomaticCraftingTableItemHandler()
+        {
+            super(9);
+        }
+    }
+}
